13f41ef5c99b45de24452b6c0aff569d6137a0ae
[Vince] <[korosi.vince12@gmail.com]>
2023-02-02 18:53:56 +0100

    push

--- FinanceLogger/src/app/list/list.component.ts
+++ FinanceLogger/src/app/list/list.component.ts
@@ -9,9 +9,9 @@ import { AddComponent } from '../add/add.component';
 export class ListComponent extends AddComponent {
   updateList(): any {
     if (this.finance.value.type === 'invoice') {
-      return `${this.finance.value.tofrom} owes ${this.finance.value.amount} Ft for ${this.finance.value.details}`;
+      return console.warn(`${this.finance.value.tofrom} owes ${this.finance.value.amount} Ft for ${this.finance.value.details}`);
     } else if (this.finance.value.type === 'payment') {
-      return `Have pay to ${this.finance.value.tofrom} ${this.finance.value.amount} Ft for ${this.finance.value.details}`;
+      return console.warn(`Have pay to ${this.finance.value.tofrom} ${this.finance.value.amount} Ft for ${this.finance.value.details}`);
     }
   }
 }
