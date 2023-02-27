9cb707d14fffd4a29a26ef60eb834197ecab5d8b
[Vinely003] <[korosi.vince12@gmail.com]>
2023-02-27 19:58:07 +0100

    modified

--- FinanceLogger/src/app/add/add.component.ts
+++ FinanceLogger/src/app/add/add.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../data-service.service';
 import { FormData } from 'src/form-data';
 
@@ -14,14 +14,17 @@ export class AddComponent {
 
   constructor(private dataService: DataService) {
     this.finance = new FormGroup({
-      type: new FormControl(''),
-      tofrom: new FormControl(''),
-      details: new FormControl(''),
-      amount: new FormControl(''),
+      type: new FormControl('', Validators.required),
+      tofrom: new FormControl('', Validators.required),
+      details: new FormControl('', Validators.required),
+      amount: new FormControl('', Validators.required),
     });
   }
 
   onSubmit(): void {
+    if (this.finance.invalid) {
+      return;
+    }
     const formValue = this.finance.value;
     const formData = new FormData(formValue.type, formValue.tofrom, formValue.details, formValue.amount);
     this.dataService.setFormData(formData);
